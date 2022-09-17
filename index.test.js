043ba025ee19b4088c7f69bd30b5c1452c55b7b1
Frozen FIsh <976499226@qq.com>
2022-09-17 22:32:05 +0800

    chore: update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,6 +1,15 @@
 import { describe, expect, it } from 'vitest'
 describe("parse",()=>{
 
+  it('match',()=>{
+    let i = 0;
+    const content = "<div>hello</div>"
+    function match(str){
+      return content.slice(i,str.length) === str
+    }
+    expect(match("<div>")).toBe(true)
+  })
+
   it('exported', () => {
     expect(1).toEqual(1)
   })
