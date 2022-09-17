dacfbff285030d41246df628c90bb1d66f54746c
Frozen FIsh <976499226@qq.com>
2022-09-17 22:48:53 +0800

    chore: add test

--- index.test.js
+++ index.test.js
@@ -1,20 +1,55 @@
-import { describe, expect, it } from 'vitest'
-describe("parse",()=>{
+import {describe, expect, it} from 'vitest'
 
-  it('match',()=>{
+describe("parse", () => {
+
+  it('match', () => {
     let i = 0;
     const content = "<div>hello</div>"
-    function match(str){
-      return content.slice(i,str.length) === str
+    function match(str) {
+      return content.slice(i, str.length) === str
     }
+
     expect(match("<div>")).toBe(true)
+  })
+
+  it('eat', () => {
+    let i = 0;
+    const content = "<div>hello</div>"
+    function match(str) {
+      return content.slice(i, str.length) === str
+    }
+    function eat(str) {
+      if (match(str)) {
+        i += str.length
+      } else {
+        throw new Error(`Parse error: expecting "${str}"`)
+      }
+    }
+    eat("<div>")
+    expect(i).toEqual(5)
+  })
+
+  it('eat error', () => {
+    let i = 0;
+    const content = "<div>hello</div>"
+    function match(str) {
+      return content.slice(i, str.length) === str
+    }
+    function eat(str) {
+      if (match(str)) {
+        i += str.length
+      } else {
+        throw new Error(`Parse error: expecting "${str}"`)
+      }
+    }
+    expect(()=>eat("<button>")).toThrowError()
   })
 
   it('exported', () => {
     expect(1).toEqual(1)
   })
 
-  it("parse fragment",()=>{
+  it("parse fragment", () => {
 
   })
 })
