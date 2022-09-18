07a639beca50db6b8fb8f093f997494db4d0c039
Frozen FIsh <976499226@qq.com>
2022-09-19 00:44:51 +0800

    chore: update app.svelte

--- src/app.svelte
+++ src/app.svelte
@@ -3,7 +3,6 @@
   const increment = () => counter++;
   const decrement = () => counter--;
 </script>
-
 <button on:click={decrement}>Decrement</button>
 <div>{counter}</div>
 <button on:click={increment}>Increment</button>
